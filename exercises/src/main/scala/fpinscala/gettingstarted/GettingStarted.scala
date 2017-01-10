21352512162a32d60809ad160780780085da0fdd
Vishal Lama <vishal.lama@gmail.com>
2017-01-10 16:18:11 -0700

    Complete Ex 2.1 (tail recursive fibonacci)

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,14 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, current: Int, next: Int): Int =
+      if (n == 0) current
+      else go(n-1, next, current + next)
+
+    go(n, 0, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
