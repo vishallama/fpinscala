a4ba840a2d41963f68751728e3948f797c1e4ab5
Vishal Lama <vishal.lama@gmail.com>
2017-01-11 10:41:32 -0700

    Complete Ex 3.3 (setHead of a list)

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -58,7 +58,11 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(_, t) => t
     }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => sys.error("setHead on empty list")
+      case Cons(_, t) => Cons(h, t)
+    }
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
