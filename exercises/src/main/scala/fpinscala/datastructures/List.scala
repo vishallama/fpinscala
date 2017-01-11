5a96cec2ca46c6f84377efb62ca67651bbf0d527
Vishal Lama <vishal.lama@gmail.com>
2017-01-11 09:51:41 -0700

    Complete Ex 3.1 (pattern matching)

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -31,6 +31,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     case _ => 101
   }
 
+  // x = 3
+
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
