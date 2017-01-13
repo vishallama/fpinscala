4c1b1f4ec2698b3df987759b3e6f14abe8b5a228
Vishal Lama <vishal.lama@gmail.com>
2017-01-13 06:35:48 -0700

    Complete Ex 3.20 (flatMap using concat and map)

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -124,4 +124,7 @@ object List { // `List` companion object. Contains functions for creating and wo
   def filter[A](as: List[A])(f: A => Boolean): List[A] =
     foldRight(as, Nil: List[A])(
       (h, t) => if (f(h)) Cons(h, t) else t)
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    concat(map(as)(f))
 }
