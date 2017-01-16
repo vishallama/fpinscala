7e6d5f5aecbe195bb7bbb16d4120cc5df0338443
Vishal Lama <vishal.lama@gmail.com>
2017-01-16 15:51:35 -0700

    Ex 4.3 - Implement map2 using flatMap and map

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -52,7 +52,8 @@ object Option {
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(x => b.map(y => f(x, y)))
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
