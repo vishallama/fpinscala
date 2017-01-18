fc01862762d55f6b90af272a53794ebd5972e891
Vishal Lama <vishal.lama@gmail.com>
2017-01-18 14:51:56 -0700

    Ex 5.7 - Implement flatMap using foldRight

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -67,6 +67,9 @@ trait Stream[+A] {
   def append[B >: A](s: => Stream[B]): Stream[B] =
     foldRight(s)((h, t) => cons(h, t))
 
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((h, t) => f(h).append(t))
+
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
 }
 case object Empty extends Stream[Nothing]
