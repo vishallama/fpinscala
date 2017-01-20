1f407effe86f94630bf15e6dac477f2b0ff4004e
Vishal Lama <vishal.lama@gmail.com>
2017-01-20 14:04:19 -0700

    Ex 6.1 - Implement nonNegativeInt

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -30,7 +30,10 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
 
   def double(rng: RNG): (Double, RNG) = ???
 
