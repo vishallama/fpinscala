6325d3528c9958864079dfb8d8f8cb99fd0dbe7a
Vishal Lama <vishal.lama@gmail.com>
2017-01-20 14:08:59 -0700

    Ex 6.2 - Implement double

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -35,7 +35,10 @@ object RNG {
     (if (i < 0) -(i + 1) else i, r)
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = ???
 
