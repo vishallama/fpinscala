f4406be9c70f76423ce60368286617af3fb20936
Vishal Lama <vishal.lama@gmail.com>
2017-01-31 19:03:23 -0700

    Ex 6.7 - Implement ints using sequence and int

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -72,6 +72,9 @@ object RNG {
     loop(count, rng, Nil)
   }
 
+  def intsViaSequence(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(int))
+
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     rng => {
       val (a, r1) = ra(rng)
